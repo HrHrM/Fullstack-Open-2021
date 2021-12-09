efbbcaf5588222d6f7bb3e542002243954cb07d2
Johnny <johnny130205@gmail.com>
2021-12-09 17:01:25 -0400

    Update App.jsx

--- FullStack-Open-2021-Course/src/App.jsx
+++ FullStack-Open-2021-Course/src/App.jsx
@@ -2,44 +2,72 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  
+  console.log('Initialized phos')
+  const now = new Date()
+  console.log(now)
+  const a = 10
+  const b = 20
+  console.log(`The total is ${a + b}`)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <p>
+        Phosphophyllite, it is now {now.toDateString()} 
+      </p>
+      <p>
+        {a} plus {b} is equal to {a + b}
+        
+      </p>
     </div>
+
   )
 }
 
 export default App
+
+
+//------------------Codigo Base de la aplicacion----------------------------
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello Vite + React!</p>
+//         <p>
+//           <button type="button" onClick={() => setCount((count) => count + 1)}>
+//             count is: {count}
+//           </button>
+//         </p>
+//         <p>
+//           Edit <code>App.jsx</code> and save to test HMR updates.
+//         </p>
+//         <p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//           {' | '}
+//           <a
+//             className="App-link"
+//             href="https://vitejs.dev/guide/features.html"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Vite Docs
+//           </a>
+//         </p>
+//       </header>
+//     </div>
+//   )
+// }
+
+// export default App
