e245efee9b53fec7662e7b9a21a866eb70598fab
Johnny <johnny130205@gmail.com>
2021-12-16 11:24:35 -0400

    Started phonebook 2.6

--- FullStack-Open-2021-Course/src/App.jsx
+++ FullStack-Open-2021-Course/src/App.jsx
@@ -1,83 +1,120 @@
-import { useState } from 'react'
-import logo         from './logo.svg'
+import React, { useState } from 'react'
+import logo                from './logo.svg'
 import './App.css'
+import Notes               from './Components/Notes'
 
-const App = ({notes}) => {
-  
-  const mapped = notes.map(e => {
+const App = () => {
+
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Phosphophyllite'}
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('added new user', event.target)
+
+    const newPerson = {
+      name: newName
+    }
+    console.log(newPerson)
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+  const handlePerson = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const personsMap = persons.map(e => {
     return(
-      <Notes key={e.id} note = {e} />
+      <Notes data = {e.name} key = {e.name} />
     )
   })
 
-  return(
+  return (
     <div>
-      <h1> Notes </h1>
-      <ul>
-        {mapped}
-      </ul>
+      <h2>Phonebook</h2>
+      <form onSubmit = {addPerson} >
+        <div>
+          name: <input value    = {newName}
+                       onChange = {handlePerson}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personsMap}
+      </div>
+      
     </div>
   )
 }
 
-export default App
+export default App  
 
-const Notes = ({ note }) => {
-  return <li> {note.content} </li>
-}
-
-
-export default App
-
-//----------------------App base Parte 2----------------------
-// const App = () => {
-
-//   const course = {
-//     name: 'Half Stack application development',
-//     parts: [
-//       {
-//         name: 'Fundamentals of React',
-//         exercises: 10
-//       },
-//       {
-//         name: 'Using props to pass data',
-//         exercises: 7
-//       },
-//       {
-//         name: 'State of a component',
-//         exercises: 14
-//       }
-//     ]
-//   }
-
-
-//   return (
-//     <div>
-//       <Header course = {course.name} />
-//       <Content parts = {course.parts} />
-//       <Footer parts = {course.parts} /> 
-//     </div>
-//   )
-// }
-
-// export default App
 
 //---------------------Notes----------------------
 
-// const App = ({notes}) => {
+// const App = ({ content }) => {
+
+//   const [ notes, setNotes ]     = useState(content)
+//   const [ newNote, setNewNote ] = useState('')
+//   const [ showAll, setShowAll ] = useState([])
+
   
-//   const mapped = notes.map(e => {
-//     return(
-//       <Notes key={e.id} note = {e} />
-//     )
+//   const addNote = (event) => {
+//     event.preventDefault()
+    
+//     console.log('Button Clicked', event.target)
+//     const noteObject = {
+//       id:        notes.length + 1,
+//       content:   newNote,
+//       date:      new Date().toISOString,
+//       important: Math.random() < 0.5,
+//     }
+//     console.log(noteObject)
+//     setNotes(notes.concat(noteObject))
+//     setNewNote('')
+//   }
+
+//   const handleNoteChange = (event) => {
+//     console.log(event.target.value)
+//     setNewNote(event.target.value)
+//   }
+//   const handleShowChange = () => {
+//     setShowAll(!showAll)
+//   }
+
+//   const notesShow = showAll ? notes : notes.filter(note => note.important === true)
+//   const notesShowMap = notesShow.map(e => {
+//       return(
+//         <Notes key={e.id} note = {e} />
+//       )
 //   })
 
 //   return(
 //     <div>
 //       <h1> Notes </h1>
+//       <div>
+//         <button onClick={handleShowChange}>
+//           Show {showAll ? 'important' : 'all' }
+//         </button>
+//       </div>
 //       <ul>
-//         {mapped}
+//         {notesShowMap}
 //       </ul>
+//       <form onSubmit={addNote} >
+//         <input value    = {newNote}
+//                onChange = {handleNoteChange} />
+//         <button type='submit' >
+//           save
+//         </button>
+//       </form>
 //     </div>
 //   )
 // }
@@ -87,4 +124,3 @@ export default App
 // const Notes = ({ note }) => {
 //   return <li> {note.content} </li>
 // }
-
---
+++ FullStack-Open-2021-Course/src/Components/Notes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Notes = ({data}) => {
+    return(
+        <li>
+            {data}
+        </li>
+    )
+}
+
+export default Notes
