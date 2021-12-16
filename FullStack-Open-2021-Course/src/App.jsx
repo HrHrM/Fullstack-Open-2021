76f8da21583754b9a3ed9107a3b817e52d08ecc9
Johnny <johnny130205@gmail.com>
2021-12-16 09:19:53 -0400

    added main to part 2 - 1

--- FullStack-Open-2021-Course/src/App.jsx
+++ FullStack-Open-2021-Course/src/App.jsx
@@ -1,68 +1,31 @@
 import { useState } from 'react'
 import logo         from './logo.svg'
 import './App.css'
-import Course       from './Components/Course'
 
-
-const App = () => {
-
-  const course = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
-
-  const mapped = course.map(e => {
+const App = ({notes}) => {
+  
+  const mapped = notes.map(e => {
     return(
-      <Course key = {e.id} course = {e} />
+      <Notes key={e.id} note = {e} />
     )
   })
 
-  return (
+  return(
     <div>
-      {mapped}
+      <h1> Notes </h1>
+      <ul>
+        {mapped}
+      </ul>
     </div>
   )
 }
+
+export default App
+
+const Notes = ({ note }) => {
+  return <li> {note.content} </li>
+}
+
 
 export default App
 
