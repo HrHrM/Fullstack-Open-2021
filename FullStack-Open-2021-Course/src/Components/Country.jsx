df695720daa8ae443ba96a0d6c836eecf9a48496
Johnny <johnny130205@gmail.com>
2021-12-22 11:54:18 -0400

    finished 2.14

--- FullStack-Open-2021-Course/src/Components/Country.jsx
+++ FullStack-Open-2021-Course/src/Components/Country.jsx
@@ -1,5 +1,6 @@
 import React, {
-    useState
+    useState,
+    useEffect
 }             from "react";
 import Button from "./Button";
 import axios  from "axios";
@@ -12,22 +13,23 @@ const Country = ({country, countryShow, setShow}) => {
     const [ windSpeed, setWindSpeed ]         = useState('')
     const [ windDirection, setWindDirection ] = useState('')
 
-    // const api_key = process.env.WEATHER_API
-    // const weather_api = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+    const api_key = import.meta.env.VITE_WEATHER_KEY
+    const weather_api = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
 
     const lang    = Object.values(country.languages).map(e => {
         return(
             <li key={e}> {e} </li>
         )
     })
-    // useEffect(() => {
-    //     axios.get(weather_api).then(response => {
-    //       setWeatherIcon  (response.data['current']['weather_icons'][0])
-    //       setTemperature  (response.data['current']['temperature'])
-    //       setWindSpeed    (response.data['current']['wind_speed'])
-    //       setWindDirection(response.data['current']['wind_dir'])
-    //     })
-    //   }, [weather_api])
+
+    useEffect(() => {
+        axios.get(weather_api).then(response => {
+          setWeatherIcon  (response.data['current']['weather_icons'][0])
+          setTemperature  (response.data['current']['temperature'])
+          setWindSpeed    (response.data['current']['wind_speed'])
+          setWindDirection(response.data['current']['wind_dir'])
+        })
+      }, [weather_api])
 
     if(countryShow[country.name.common]) {
         return(
@@ -46,6 +48,25 @@ const Country = ({country, countryShow, setShow}) => {
                 <div>
                     <img src={country.flags.svg} alt="Country Flag" width='20%' />
                 </div>
+                <div>
+                    <h3> Weather in {country.capital} </h3>
+                    <div>
+                        <div>
+                            <h4> Temperature </h4>
+                            <div>
+                                {temperature}° Celcius
+                            </div>
+                            <br/>
+                            <img src={weatherIcon} alt="Weather Icon" width="10%" />
+                        </div>
+                        <div>
+                            <h4> Wind </h4>
+                            <div> Speed: {windSpeed} mph </div>
+                            <div> Direction: {windDirection} </div>
+                        </div>
+                        <br/>
+                    </div>
+                </div>
             </div>
         )
 
@@ -55,7 +76,7 @@ const Country = ({country, countryShow, setShow}) => {
         <div>
             {country.name.common}
             <Button countryShow = {countryShow} setShow = {setShow} name = {country.name.common} />
-            
+
         </div>
     )
 
